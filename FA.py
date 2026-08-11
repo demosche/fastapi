a8b74b419824afe356fb7e49fc571f39e9105d4a
demosche <misan3760@gmail.com>
2026-08-11 16:13:26 +0500

    created endpoint /sum

--- FA.py
+++ FA.py
@@ -7,5 +7,9 @@ app = FastAPI()
 def hello():
     return {"message": "Hello WorldFA"}
 
+@app.get("/sum")
+def sum_numbers(a: int, b: int):
+    return {"result": a + b}
+
 if __name__ == "__main__":
     uvicorn.run("FA:app", host="0.0.0.0", port=8000, reload=True)
