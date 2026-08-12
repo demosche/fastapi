437e89d294c5f0c7fb8b3e4aa99df24f00220ad3
demosche <misan3760@gmail.com>
2026-08-12 15:38:27 +0500

    created postSum

--- FA.py
+++ FA.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
+
+
+class Sumy(Basemodel):
+    a: int
+    b: int    
+
 
 app = FastAPI()
 
@@ -7,9 +14,13 @@ app = FastAPI()
 def hello():
     return {"message": "Hello WorldFA"}
 
-@app.get("/sum")
-def sum_numbers(a: int, b: int):
+@app.get("/sumGet")
+def summy(a: int, b: int):
     return {"result": a + b}
+
+@app.post("/sumPost")
+async def summyPost(sum:Sumy):
+    return {"return": sum.a + sum.b}
 
 if __name__ == "__main__":
     uvicorn.run("FA:app", host="0.0.0.0", port=8000, reload=True)
